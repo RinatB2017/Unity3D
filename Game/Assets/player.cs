1596286634e3ccb99c82163458b5accc3e067d0b
RinatB2017 <tux4096@gmail.com>
2017-12-08 02:35:16 +0300

    update

--- Game/Assets/player.cs
+++ Game/Assets/player.cs
@@ -3,29 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
+	bool flag_left = false;
+	bool flag_right = false;
+	bool flag_up = false;
+	bool flag_down = false;
+
+	float X = 0;
+	float Y = 0;
+	float Z = 0;
+
+	float delta = 0.1f;
 
 	// Use this for initialization
 	void Start () {
-		
+		X = transform.position.x;
+		Y = transform.position.y;
+		Z = transform.position.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			if (transform.position.x < Screen.width)
-			{
-				transform.position = new Vector2(transform.position.x + 0.1f,
-				                                 transform.position.y);
-			}
-		}
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			if (transform.position.x > 0)
-			{
-				transform.position = new Vector2(transform.position.x - 0.1f,
-												 transform.position.y);
-			}
-		}
+		float tX = Input.GetAxis("Horizontal");
+		float tY = Input.GetAxis("Vertical");
+		if (tX > 0)	X -= delta;
+		if (tX < 0) X += delta;
+		if (tY > 0) Y -= delta;
+		if (tY < 0) Y += delta;
+
+		transform.position = new Vector3(X, Y, Z);
 	}
 }
