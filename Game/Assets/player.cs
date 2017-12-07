cb412d1d3bce2b0d295824a8b48b898a569c158f
RinatB2017 <tux4096@gmail.com>
2017-12-08 01:59:34 +0300

    update

---
+++ Game/Assets/player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			if (transform.position.x < Screen.width)
+			{
+				transform.position = new Vector2(transform.position.x + 0.1f,
+				                                 transform.position.y);
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			if (transform.position.x > 0)
+			{
+				transform.position = new Vector2(transform.position.x - 0.1f,
+												 transform.position.y);
+			}
+		}
+	}
+}
