fc254b0b347bcbf4554fb893642288ba2b8a25af
RinatB2017 <tux4096@gmail.com>
2017-12-08 00:40:41 +0300

    update

---
+++ MyFirstProject/Assets/GenerateObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateObjects : MonoBehaviour {
+	public GameObject circle;
+
+	void AddGameObject()
+	{
+		Instantiate(circle);
+	}
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("AddGameObject", 1f, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MyFirstProject/Assets/player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour {
+
+	bool flag = false;
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "ground")
+		{
+			flag = true;
+		}
+		if (col.gameObject.tag == "bag")
+		{
+			// game over
+			SceneManager.LoadScene("scene2");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (flag && Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 500));
+			flag = false;
+		}	
+	}
+}
