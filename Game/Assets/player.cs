92aaad9a0394ab58c6a5c36b748c881c4f5df8ae
RinatB2017 <tux4096@gmail.com>
2017-12-09 23:32:05 +0300

    update

--- Game/Assets/player.cs
+++ Game/Assets/player.cs
@@ -14,11 +14,18 @@ public class player : MonoBehaviour {
 
 	float delta = 0.1f;
 
+	public GameObject bullet;
+
 	// Use this for initialization
 	void Start () {
 		X = transform.position.x;
 		Y = transform.position.y;
 		Z = transform.position.z;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(0, 0, 100, 100), "Z=" + Z);
 	}
 	
 	// Update is called once per frame
@@ -35,6 +42,20 @@ public class player : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			//GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 500));
+			//bullet.transform.position = new Vector3(10, 10, 0);
+
+			//GameObject bullet = GameObject.Instantiate(bullet);
+			//bullet.tag = "bullet";
+			//Instantiate(Resources.Load("bullet"));
+			//Instantiate(bullet);
+
+			bullet = new GameObject("bullet");
+			bullet.transform.position = new Vector3(X, Y, Z);
+			bullet.transform.localScale = new Vector3(10, 10, 10);
+
+			//GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			//cube.AddComponent<Rigidbody>();
+			//cube.transform.position = new Vector3(5, 5, 0);
 		}
 	}
 }
