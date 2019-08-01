c1e34d6cd9fae5cf1d2b4800085f5aa5b5d6a428
RinatB2017 <tux4096@gmail.com>
2019-08-02 00:56:36 +0300

    update

--- my_projects/P_000/Assets/Scripts/Test_click.cs
+++ my_projects/P_000/Assets/Scripts/Test_click.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Test_click : MonoBehaviour
 {
+    [SerializeField]
+    private float XXX = 1.0F;
+    
     void OnMouseDown()
     {
         Debug.Log("click");
