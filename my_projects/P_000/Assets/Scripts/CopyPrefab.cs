4f0570ba8c16911ba72ac8afaea26dcdb7da6abc
RinatB2017 <tux4096@gmail.com>
2019-08-02 17:08:26 +0300

    update

---
+++ my_projects/P_000/Assets/Scripts/CopyPrefab.cs
@@ -0,0 +1,16 @@
+// Instantiates 10 copies of Prefab each 2 units apart from each other
+
+using UnityEngine;
+using System.Collections;
+
+public class CopyPrefab : MonoBehaviour
+{
+    public Transform prefab;
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }
+    }
+}
