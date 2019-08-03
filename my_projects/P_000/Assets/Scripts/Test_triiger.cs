0f49cede1f782d0da9b1de35e833fcd6ff32272a
RinatB2017 <tux4096@gmail.com>
2019-08-03 22:43:35 +0300

    update

--- my_projects/P_000/Assets/Scripts/Test_triiger.cs
+++ my_projects/P_000/Assets/Scripts/Test_triiger.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Test_triiger : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("OnTriggerEnter");
+        SceneManager.LoadScene("Scene_002");
     }
     
     void OnTriggerExit2D(Collider2D other)
