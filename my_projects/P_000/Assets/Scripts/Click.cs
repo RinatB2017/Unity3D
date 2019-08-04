87c033f7397a65a508ea6d0ea883ea2c55bd7821
RinatB2017 <tux4096@gmail.com>
2019-08-04 22:22:28 +0300

    update

---
+++ my_projects/P_000/Assets/Scripts/Click.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("down");
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("up");
+    }
+}
---
+++ my_projects/P_001/Assets/Scripts/Click.cs
@@ -0,0 +1,23 @@
+// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public Transform prefab;
+    void Update()
+    {
+        
+    }
+    
+    void OnMouseDown()
+    {
+        Debug.Log("down");
+        Instantiate(prefab, new Vector3(1, 1, 0), Quaternion.identity);
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("up");
+    }
+}
