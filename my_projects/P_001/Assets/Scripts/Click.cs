9ebd0c314731ba66fba44a70fb27fa55c06626fb
RinatB2017 <tux4096@gmail.com>
2019-08-08 02:14:44 +0300

    update

--- my_projects/P_001/Assets/Scripts/Click.cs
+++ my_projects/P_001/Assets/Scripts/Click.cs
@@ -1,15 +1,34 @@
-// using System.Collections;
-// using System.Collections.Generic;
 using UnityEngine;
 
 public class Click : MonoBehaviour
 {
     public Transform prefab;
     
+    private void find_obj()
+    {
+        Tux tux = (Tux)FindObjectOfType(typeof(Tux));
+        if(tux)
+        {
+            print("value = " + tux.get_value());
+            print("name  = " + tux.get_name());
+        }
+    }
+
+    private void find_obj2()
+    {
+        var tux = FindObjectsOfType<Tux>();
+        if(tux.Length != 0)
+        {
+            print("value = " + tux[0].get_value());
+        }
+    }
+
     void OnMouseDown()
     {
         Debug.Log("down");
-        Instantiate(prefab, new Vector3(1, 1, 0), Quaternion.identity);
+        //Instantiate(prefab, new Vector3(1, 1, 0), Quaternion.identity);
+
+        find_obj();
     }
 
     void OnMouseUp()
