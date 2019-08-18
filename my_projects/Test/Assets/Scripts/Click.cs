5bb06be780fe36463557c14fa0ccb0ec26bfafca
RinatB2017 <tux4096@gmail.com>
2019-08-18 15:55:54 +0300

    update

--- my_projects/Test/Assets/Scripts/Click.cs
+++ my_projects/Test/Assets/Scripts/Click.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Click : MonoBehaviour
 {
-    public string s_name;
+    public string   s_name;
+    public Text     s_text;
 
     void OnMouseDown()
     {
         Debug.Log("down " + s_name);
+        s_text.text = s_name +" down";
     }
 
     void OnMouseUp()
     {
         Debug.Log("up " + s_name);
+        s_text.text = s_name +" up";
     }
 }
