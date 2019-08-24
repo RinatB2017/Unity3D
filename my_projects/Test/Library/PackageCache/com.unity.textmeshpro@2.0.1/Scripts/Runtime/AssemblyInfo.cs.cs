cd930b116be678f2ff8fc4d57bf6b848f4939019
RinatB2017 <tux4096@gmail.com>
2019-08-24 14:33:54 +0300

    update

--- my_projects/Test/Library/PackageCache/com.unity.textmeshpro@2.0.1/Scripts/Runtime/AssemblyInfo.cs.cs
+++ my_projects/Test/Library/PackageCache/com.unity.textmeshpro@2.0.1/Scripts/Runtime/AssemblyInfo.cs.cs
@@ -1,4 +1,4 @@
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 // Allow internal visibility for testing purposes.
 [assembly: InternalsVisibleTo("Unity.TextCore")]
