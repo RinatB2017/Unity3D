2c681dbc6ca647b51d5c1e913e4f332ba1906cb2
RinatB2017 <tux4096@gmail.com>
2019-08-24 13:59:33 +0300

    update

--- my_projects/Test2/Assets/Scripts/Bomb.cs
+++ my_projects/Test2/Assets/Scripts/Bomb.cs
@@ -1,19 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     void OnTriggerExit2D(Collider2D other) 
 	{
 		if (other.gameObject.tag == "Boundary" && other.gameObject.name != "Top Boundary") 
---
+++ my_projects/Test2/Assets/Scripts/Spawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Rigidbody2D obj;
+    private Vector2 pos;
+
+    void Awake()
+    {
+        pos = obj.position;
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+	{
+        obj.MovePosition(pos);
+    }
+}
