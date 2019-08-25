9aa36b096dd48ea657f73aad69bed10f0d840b54
RinatB2017 <tux4096@gmail.com>
2019-08-26 01:55:41 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/Splash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    public float delay_sec = 2.5f;
+    private IEnumerator coroutine;
+
+    void Start()
+    {
+        coroutine = DropBombes();
+        StartCoroutine(coroutine);        
+    }
+
+	private IEnumerator DropBombes () 
+	{
+        yield return new WaitForSeconds(delay_sec);
+        Destroy(gameObject);
+    }
+}
