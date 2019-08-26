3b6dfd7d8a74ae7db812c670775ec2d2db1ad498
RinatB2017 <tux4096@gmail.com>
2019-08-27 00:02:33 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/GetImpulse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GetImpulse : MonoBehaviour
+{
+    private Vector3 m_NewForce;
+    private Rigidbody2D Rigid;
+
+    void Awake() 
+    {
+        m_NewForce.x = 0;
+        m_NewForce.y = 20;
+        m_NewForce.z = 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.gameObject.tag == "Ball") 
+		{
+            Rigid = other.GetComponent<Rigidbody2D>();
+
+            Rigid.AddForce(m_NewForce, ForceMode2D.Impulse);
+        }
+	}
+}
