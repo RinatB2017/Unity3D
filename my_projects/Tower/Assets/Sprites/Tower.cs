6a3b97debb1fce7ab67f3355eb02dae888c31cf3
RinatB2017 <tux4096@gmail.com>
2019-09-07 13:13:54 +0300

    update

---
+++ my_projects/Tower/Assets/Sprites/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public Transform prefab;
+
+    void Start()
+    {
+        for(float z=0.5f; z<3.5f; z+=1.0f)
+        {
+            for(float y=0.5f; y<5.5f; y+=1.0f)
+            {
+                for(float x=0.5f; x<3.5f; x+=1.0f)
+                {
+                    Vector3 temp_vector = new Vector3(x, y, z);
+                    Instantiate(prefab, temp_vector, Quaternion.identity);
+                }
+            }
+        }
+        // Vector3 temp_vector = new Vector3(1, 0.5f, 1);
+        // Instantiate(prefab, temp_vector, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
