0789f7a9cea6ee2d048da1bd2755ffa2e013185b
RinatB2017 <tux4096@gmail.com>
2019-09-11 01:55:47 +0300

    update

---
+++ my_projects/Nucleus/Assets/Scriptes/FPSDisplay.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSCounter))]
+public class FPSDisplay : MonoBehaviour 
+{
+	public Text fpsLabel;
+        
+    FPSCounter fpsCounter;
+
+	void Awake () 
+    {
+		fpsCounter = GetComponent<FPSCounter>();
+	}
+
+	void Update () 
+    {
+		//fpsLabel.text = fpsCounter.FPS.ToString();
+		fpsLabel.text = Mathf.Clamp(fpsCounter.FPS, 0, 99).ToString();
+	}
+}
