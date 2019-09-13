9d3c8026d34dd5d92ea4ff9363cfe05f9f46ecbd
RinatB2017 <tux4096@gmail.com>
2019-09-13 22:22:44 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/RunPage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunPage : MonoBehaviour
+{
+    public GameObject page;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = page.transform.position;
+            GameObject obj = Instantiate(page, pos, Quaternion.identity);
+
+            Destroy(gameObject);
+        }        
+    }
+}
