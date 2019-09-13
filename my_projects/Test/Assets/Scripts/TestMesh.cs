b5822befed9d4e6ed606886cb0cfc55f5e79d2e5
RinatB2017 <tux4096@gmail.com>
2019-09-14 00:01:16 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/TestMesh.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMesh : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<TextMesh>().text = "Hello World";
+    }
+}
