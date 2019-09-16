f585c2580eb8e3c23bc0d8734daeb58342f347c9
RinatB2017 <tux4096@gmail.com>
2019-09-17 01:20:28 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/CheckColliders.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckColliders : DebugClass
+{
+    void OnMouseDown()
+    {
+        debug_print("OnMouseDown");
+    }
+
+    void OnMouseUp()
+    {
+        debug_print("OnMouseUp");
+    }
+}
