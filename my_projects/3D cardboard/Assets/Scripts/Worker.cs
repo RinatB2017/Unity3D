9119cd967172f8343ce053fa82607d70525fb4e8
RinatB2017 <tux4096@gmail.com>
2019-09-18 01:30:45 +0300

    update

---
+++ my_projects/3D cardboard/Assets/Scripts/Worker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worker : MonoBehaviour
+{
+    public GameObject l_prefab;
+    public GameObject r_prefab;
+
+    void Start()
+    {
+        float xAngle = 0f;
+        float yAngle = 45f;
+        float zAngle = 0f;
+
+        l_prefab.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+        r_prefab.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+    }
+
+    void Update()
+    {
+        
+    }
+}
