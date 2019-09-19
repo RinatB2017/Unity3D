9bad883ca66e8d4ef643d4aa808610e39b99185c
RinatB2017 <tux4096@gmail.com>
2019-09-19 14:58:40 +0300

    update

--- my_projects/Test/Assets/Scripts/Events/SendEvent.cs
+++ my_projects/Test/Assets/Scripts/Events/SendEvent.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class SendEvent : MonoBehaviour
 {
+    void OnEnable()
+    {
+        print("OnEnable");
+    }
+    void OnDisable()
+    {
+        print("OnDisable");
+    }
+
     void OnMouseDown()
     {
         float x = 1f / 3f;
