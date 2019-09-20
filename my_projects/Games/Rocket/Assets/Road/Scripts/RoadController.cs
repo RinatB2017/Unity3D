58b16490fadb56a36e24ff8d20500401c5159b99
RinatB2017 <tux4096@gmail.com>
2019-09-20 17:10:28 +0300

    update

---
+++ my_projects/Games/Rocket/Assets/Road/Scripts/RoadController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadController : DebugClass
+{
+    public List<GameObject> prefabs;
+    public Vector2 position;
+    public float step = 0.1f;
+
+    private List<GameObject> l_obj;
+    private float h_road = 0f;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector2 pos = col.transform.position;
+        pos.y += h_road;
+
+        col.transform.position = pos;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Vector2 pos = col.transform.position;
+        pos.y += h_road;
+
+        col.transform.position = pos;
+    }
+
+    void Start()
+    {
+        l_obj = new List<GameObject>();
+
+        position.x = 0f;
+        position.y = 0f;
+
+        for(int n=0; n<prefabs.Count; n++)
+        {
+            GameObject obj = Instantiate(prefabs[n], position, Quaternion.identity);
+            float h = prefabs[n].GetComponent<SpriteRenderer>().bounds.size.y;
+            h_road += h;
+            position.y += h;
+            l_obj.Add(obj);
+        }
+    }
+
+    void move()
+    {
+        Vector2 temp_pos;
+        for(int n=0; n<l_obj.Count; n++)
+        {
+            temp_pos = l_obj[n].transform.position;
+            temp_pos.y -= step;
+
+            l_obj[n].transform.position = temp_pos;
+        }
+    }
+
+    void Update()
+    {
+        move();
+    }
+}
---
+++ my_projects/Test/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    Camera m_MainCamera;
+    float k = 0.1f;
+
+    void Start()
+    {
+        m_MainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        m_MainCamera.transform.Translate (Input.GetAxis("Horizontal")*k,
+                                          Input.GetAxis("Vertical")*k,
+                                          0);
+    }
+}
