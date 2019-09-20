8bfaf0030ef75ae9f1153646162d79b5f3e79976
RinatB2017 <tux4096@gmail.com>
2019-09-20 14:53:52 +0300

    update

--- my_projects/Cannon/Assets/Cannon/Scripts/Cannon.cs
+++ my_projects/Cannon/Assets/Cannon/Scripts/Cannon.cs
@@ -8,8 +8,8 @@ public class Cannon : MonoBehaviour
     public GameObject cannonball;
     public Text info_angle;
     public Text info_power;
-    public float angle = 30f;
-    public float power = 100f;
+    public float angle = 30f;   //56
+    public float power = 100f;  //13
 
     void Awake()
     {
@@ -45,9 +45,32 @@ public class Cannon : MonoBehaviour
         info_power.text = power.ToString();
     }
 
+    public static Vector2 RadianToVector2(float radian)
+    {
+        return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+    }
+      
+    public static Vector2 DegreeToVector2(float degree)
+    {
+        return RadianToVector2(degree * Mathf.Deg2Rad);
+    }
+
     void Shoot()
     {
         print("Shoot");
+
+        Vector3 m_NewForce;
+
+        Vector2 vector = DegreeToVector2(angle);
+        m_NewForce.x = vector.x * power;
+        m_NewForce.y = vector.y * power;
+        m_NewForce.z = 0;
+
+        Vector3 temp_vector = transform.position;
+
+        GameObject ball;
+        ball = Instantiate(cannonball, temp_vector, Quaternion.identity);
+        ball.GetComponent<Rigidbody2D>().AddForce(m_NewForce, ForceMode2D.Impulse);
     }
 
     void Update()
