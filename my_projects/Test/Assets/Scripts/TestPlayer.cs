2e546aae0a0b501385e983748a8c8291044e58f7
RinatB2017 <tux4096@gmail.com>
2019-09-21 01:04:54 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/TestPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestPlayer : MonoBehaviour
+{
+    public Text text_x;
+    public Text text_y;
+
+    public float speed = 0.1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate (Input.GetAxis("Horizontal")*speed,
+                             Input.GetAxis("Vertical")*speed,
+                             0);
+        float l_x = transform.localPosition.x;
+        float l_y = transform.localPosition.y;
+        text_x.text = l_x.ToString();
+        text_y.text = l_y.ToString();
+    }
+}
