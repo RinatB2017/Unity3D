59ffb6b17738cd7bcbb69f616b215bdbc85eb4fa
RinatB2017 <tux4096@gmail.com>
2019-09-25 11:39:31 +0300

    update

--- my_projects/Test/Assets/Scripts/Floor.cs
+++ my_projects/Test/Assets/Scripts/Floor.cs
@@ -22,30 +22,27 @@ public class Floor : MonoBehaviour
     void up()
     {
         set_text("UP");
-        print("up");
     }
 
     void down()
     {
         set_text("DOWN");
-        print("down");
     }
 
     void left()
     {
         set_text("LEFT");
-        print("left");
     }
 
     void right()
     {
         set_text("RIGHT");
-        print("right");
     }
 
     void set_text(string text)
     {
         info.text = text;
+        print(text);
     }
 
     void Update()
