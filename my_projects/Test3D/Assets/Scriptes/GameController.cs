2d676f066f8adadea42fa022c4b4618a0c49ce24
RinatB2017 <tux4096@gmail.com>
2019-09-26 22:23:24 +0300

    update

---
+++ my_projects/Test3D/Assets/Scriptes/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject sphere;
+    public float force_x = 0f;
+    public float force_y = 10f;
+    public float force_z = 0f;
+
+    private Rigidbody body_sphere;
+    private Vector3 m_NewForce;
+
+    void Start()
+    {
+        body_sphere = sphere.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            print("Jump");
+            
+            m_NewForce.x = force_x;
+            m_NewForce.y = force_y;
+            m_NewForce.z = force_z;
+
+            body_sphere.AddForce(m_NewForce, ForceMode.Impulse);
+        }        
+    }
+}
