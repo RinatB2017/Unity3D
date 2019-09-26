96237acb9743bdf6882c1fe5be8065aca9436868
RinatB2017 <tux4096@gmail.com>
2019-09-26 19:38:06 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/Reset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    public GameObject main;
+
+    void OnMouseDown()
+    {
+        main.SendMessage("reset");
+    }
+}
