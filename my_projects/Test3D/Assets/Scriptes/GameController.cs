58799083385812b59cdd80974d643d3a824dae77
RinatB2017 <tux4096@gmail.com>
2019-09-28 00:18:56 +0300

    update

--- my_projects/Test3D/Assets/Scriptes/GameController.cs
+++ my_projects/Test3D/Assets/Scriptes/GameController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject sphere;
+    public GameObject sphere_prefab;
     public float axes_x = 0f;
     public float axes_y = 1f;
     public float axes_z = 1f;
@@ -13,6 +13,7 @@ public class GameController : MonoBehaviour
     public float k_move = 0.1f;
     public float k_rot = 0.5f;
 
+    private GameObject sphere;
     private Rigidbody body_sphere;
     private Vector3 m_NewForce;
     private Vector3 begin_pos;
@@ -28,9 +29,6 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
-        body_sphere = sphere.GetComponent<Rigidbody>();
-        begin_pos = body_sphere.transform.position;
-
         m_MainCamera = Camera.main;
 
         GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cube");
@@ -48,11 +46,13 @@ public class GameController : MonoBehaviour
     void move_forward()
     {
         print("Forward");
+        m_MainCamera.transform.Translate (0, 0, k_move);
     }
 
     void move_back()
     {
         print("Back");
+        m_MainCamera.transform.Translate (0, 0, -k_move);
     }
 
     void fire()
@@ -60,37 +60,41 @@ public class GameController : MonoBehaviour
         print("Fire");
         print("Fire");
         
-        m_NewForce.x = axes_x * force;
-        m_NewForce.y = axes_y * force;
-        m_NewForce.z = axes_z * force;
+        // m_NewForce.x = axes_x * force;
+        // m_NewForce.y = axes_y * force;
+        // m_NewForce.z = axes_z * force;
 
-        // m_NewForce.x = m_MainCamera.transform.rotation.x * force;
-        // m_NewForce.y = m_MainCamera.transform.rotation.y * force;
-        // m_NewForce.z = m_MainCamera.transform.rotation.z * force;
+        m_NewForce.x = m_MainCamera.transform.rotation.x * force;
+        m_NewForce.y = m_MainCamera.transform.rotation.y * force;
+        m_NewForce.z = m_MainCamera.transform.rotation.z * force;
 
+        Vector3 temp_vector = m_MainCamera.transform.position;
+
+        sphere = Instantiate(sphere_prefab, temp_vector, Quaternion.identity);
+        body_sphere = sphere.GetComponent<Rigidbody>();
         body_sphere.AddForce(m_NewForce, ForceMode.Impulse);
     }
 
     void Update()
     {
-        // float rotate_x = rotate_joystick.Horizontal * runSpeed;
-        // float rotate_y = rotate_joystick.Vertical * runSpeed;
-        // float move_x = move_joystick.Horizontal * runSpeed;
-        // float move_y = move_joystick.Vertical * runSpeed;
-
         // движение
-        m_MainCamera.transform.Translate (0, 0, Input.GetAxis("Vertical")*k_move);
+        // m_MainCamera.transform.Translate (0,
+        //                                   0,
+        //                                   Input.GetAxis("Vertical")*k_move);
         
         m_MainCamera.transform.Translate (move_joystick.Horizontal*k_move, 
                                           move_joystick.Vertical*k_move,
                                           0);
 
         // поворот
-        m_MainCamera.transform.Rotate(0, Input.GetAxis("Horizontal")*k_rot, 0, Space.Self);
+        // m_MainCamera.transform.Rotate(0,
+        //                               Input.GetAxis("Horizontal")*k_rot,
+        //                               0,
+        //                               Space.Self);
 
-        m_MainCamera.transform.Rotate(0,
-                                      rotate_joystick.Vertical*k_rot, 
-                                      rotate_joystick.Horizontal*k_rot,
+        m_MainCamera.transform.Rotate(rotate_joystick.Vertical*k_rot,
+                                      rotate_joystick.Horizontal*k_rot, 
+                                      0,
                                       Space.Self);
 
         if (Input.GetButtonDown("Jump"))
