72e83226313ed4b1fa2c93b24f5d2627d395ae15
RinatB2017 <tux4096@gmail.com>
2019-09-29 22:53:00 +0300

    update

--- my_projects/Games/Jump Ball/Assets/Scripts/GameController.cs
+++ my_projects/Games/Jump Ball/Assets/Scripts/GameController.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject ball_prefab;
+    private Rigidbody2D ball;
+    private Vector3 m_force;
+
+    void Awake()
+    {
+        ball = ball_prefab.GetComponent<Rigidbody2D>();
+    }
 
     void jump_left(bool state)
     {
         if(state)
         {
             print("Left");
+
+            m_force.x = ball.transform.position.x - 1f;
+            m_force.y = ball.transform.position.x;
+            m_force.z = 0;
+
+            ball.AddForce(m_force, ForceMode2D.Impulse);
         }
     }
 
@@ -19,6 +32,12 @@ public class GameController : MonoBehaviour
         if(state)
         {
             print("Right");
+
+            m_force.x = ball.transform.position.x + 1f;
+            m_force.y = ball.transform.position.x;
+            m_force.z = 0;
+
+            ball.AddForce(m_force, ForceMode2D.Impulse);
         }
     }
 }
