e45311d9a9e7000d6ab31348c5aeca995eb7f17a
RinatB2017 <tux4096@gmail.com>
2019-09-29 22:04:11 +0300

    update

---
+++ my_projects/Games/Jump Ball/Assets/Scripts/Btn_click.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class Btn_click : MonoBehaviour
+{
+    public GameObject game_controller;
+    public string f_name;
+
+    void OnMouseDown()
+    {
+        game_controller.SendMessage(f_name, true);
+    }
+
+    void OnMouseUp()
+    {
+        game_controller.SendMessage(f_name, false);
+    }
+}
---
+++ my_projects/Games/Jump Ball/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ball_prefab;
+
+    void jump_left(bool state)
+    {
+        if(state)
+        {
+            print("Left");
+        }
+    }
+
+    void jump_right(bool state)
+    {
+        if(state)
+        {
+            print("Right");
+        }
+    }
+}
