5e6ce795e4b59b35394d850ac74556ea65d0982c
RinatB2017 <tux4096@gmail.com>
2019-10-01 20:02:09 +0300

    update

--- my_projects/Test3D/Assets/Scriptes/SpawnWall.cs
+++ my_projects/Test3D/Assets/Scriptes/SpawnWall.cs
@@ -9,16 +9,22 @@ public class SpawnWall : MonoBehaviour
 
     void Start()
     {
-        Vector2 temp_vector;
-        temp_vector.y = 1f;
-        for(int y=0; y<50; y++)
+        Vector3 pos_platform = platform.transform.position;
+
+        Vector3 temp_vector;
+        temp_vector.x = pos_platform.x;
+        temp_vector.y = pos_platform.y + 1f;
+        temp_vector.z = pos_platform.z;
+
+        for(int y=0; y<5; y++)
         {
+            temp_vector.x = pos_platform.x;
             for(int cnt=0; cnt<5; cnt++)
             {
                 if(y % 2 == 0)
-                    temp_vector.x = cnt * 2;
+                    temp_vector.x += cnt * 2;
                 else
-                    temp_vector.x = cnt * 2 + 1;
+                    temp_vector.x += cnt * 2 + 1;
 
                 GameObject child = Instantiate(brick_prefab, temp_vector, Quaternion.identity);        
                 child.GetComponent<Rigidbody>().isKinematic = true;
