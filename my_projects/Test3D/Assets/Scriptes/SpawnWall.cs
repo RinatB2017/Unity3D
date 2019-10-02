de910fda27f3e65850a8ba82a631a2df081dff63
RinatB2017 <tux4096@gmail.com>
2019-10-02 03:00:38 +0300

    update

--- my_projects/Test3D/Assets/Scriptes/SpawnWall.cs
+++ my_projects/Test3D/Assets/Scriptes/SpawnWall.cs
@@ -6,6 +6,17 @@ public class SpawnWall : MonoBehaviour
 {
     public GameObject brick_prefab;
     public GameObject platform;
+    public GameObject container;
+
+    void test()
+    {
+        Vector3 temp_vector;
+        temp_vector.x = 12.5f;
+        temp_vector.y = 6.5f;
+        temp_vector.z = 17.5f;
+
+        GameObject child = Instantiate(brick_prefab, temp_vector, Quaternion.identity);
+    }
 
     void Start()
     {
@@ -16,22 +27,26 @@ public class SpawnWall : MonoBehaviour
         temp_vector.y = pos_platform.y + 1f;
         temp_vector.z = pos_platform.z;
 
+        int cnt = 0;
         for(int y=0; y<5; y++)
         {
-            temp_vector.x = pos_platform.x;
-            for(int cnt=0; cnt<5; cnt++)
+            for(int x=0; x<5; x++)
             {
                 if(y % 2 == 0)
-                    temp_vector.x += cnt * 2;
+                    temp_vector.x = pos_platform.x + x * 2;
                 else
-                    temp_vector.x += cnt * 2 + 1;
+                    temp_vector.x = pos_platform.x + x * 2 + 1;
 
-                GameObject child = Instantiate(brick_prefab, temp_vector, Quaternion.identity);        
-                child.GetComponent<Rigidbody>().isKinematic = true;
-                child.transform.SetParent(platform.transform);
+                // GameObject child = Instantiate(brick_prefab, temp_vector, Quaternion.identity);
+                GameObject child = Instantiate(brick_prefab, temp_vector, Quaternion.identity, container.transform);
+                // child.GetComponent<Rigidbody>().isKinematic = true;
+                // child.transform.SetParent(container.transform);
+                // проблема - наследуется и масштаб, в результате глюки
+                cnt++;
             }
             temp_vector.y += 1f;
         }
+        print("Cnt " + cnt);
     }
 
     void Update()
