c4c925a6dc34b282f2e2738229ec2545a71b857c
RinatB2017 <tux4096@gmail.com>
2019-10-03 20:58:22 +0300

    update

---
+++ my_projects/Test EventManager/Assets/Scripts/Test_EventManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_EventManager : MonoBehaviour
+{
+    private bool flag = false;
+    private Vector3 vector3;
+    private Vector2 vector2;
+
+    void OnEnable () 
+    {
+        EventManager.Subscribe ("event_name", MyFunction);
+    }
+
+    void OnDisable () 
+    {
+        EventManager.Unsubscribe ("event_name", MyFunction);
+    }
+
+    void MyFunction (object[] parameters) 
+    {
+	    print ("Len = " + parameters.Length);  // количество параметров
+        for(int n=0; n<parameters.Length; n++)
+        {
+	        print (parameters[n]);
+            print (parameters[n].GetType());
+        }
+    }
+
+    void Start()
+    {
+        vector3.x = 1;
+        vector3.y = 2;
+        vector3.z = 3;
+
+        vector2.x = 1;
+        vector2.y = 2;
+    }
+
+    void Update()
+    {
+        if(!flag)
+        {
+            flag = true;
+            EventManager.SendEvent ("event_name", "param_string", 1, 2f);
+            // EventManager.SendEvent ("event_name", vector3, vector2);
+            // EventManager.SendEvent ("event_name", "test");
+        }
+    }
+}
