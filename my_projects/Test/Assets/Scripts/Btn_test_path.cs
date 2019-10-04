6a8bff2ca520e5530f0d274d8a9dc6fe63ca37de
RinatB2017 <tux4096@gmail.com>
2019-10-05 01:57:56 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/Btn_test_path.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Btn_test_path : MonoBehaviour
+{
+    public PathCreator path_creator;
+
+    [ContextMenu("Do Something")]
+    void DoSomething()
+    {
+        Debug.Log("Perform operation");
+    }
+    
+    void OnMouseDown()
+    {
+        print("Click");
+        print("NumSegments " + path_creator.path.NumSegments);
+        
+        var segment = path_creator.path.GetPointsInSegment(0);
+        print(segment);
+        print(segment[0].x);
+        print(segment[0].y);
+    }
+}
