2b4a96a9308f98070737ed2c07698674ed53f587
RinatB2017 <tux4096@gmail.com>
2019-10-05 17:04:34 +0300

    update

--- my_projects/Test/Assets/Scripts/Btn_test_path.cs
+++ my_projects/Test/Assets/Scripts/Btn_test_path.cs
@@ -5,11 +5,51 @@ using UnityEngine;
 public class Btn_test_path : MonoBehaviour
 {
     public PathCreator path_creator;
+    public GameObject  ball_prefab;
+    Rigidbody2D ball_rb2D;
 
-    [ContextMenu("Do Something")]
-    void DoSomething()
+    public float speed = 10.0f;
+    private Vector2 target;
+
+    List<Vector2> l_pos;
+    int index = 0;
+
+    // [ContextMenu("Do Something")]
+    // void DoSomething()
+    // {
+    //     Debug.Log("Perform operation");
+    // }
+
+    void Start()
     {
-        Debug.Log("Perform operation");
+        ball_rb2D = ball_prefab.GetComponent<Rigidbody2D>();
+        l_pos = new List<Vector2>();
+        l_pos.Add(new Vector2(0, 1));
+        l_pos.Add(new Vector2(1, 1));
+        l_pos.Add(new Vector2(1, 0));
+        l_pos.Add(new Vector2(0, 0));
+
+        index = 0;
+        target = l_pos[index];
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+
+        // move sprite towards the target location
+        ball_prefab.transform.position = Vector2.MoveTowards(ball_prefab.transform.position, target, step);
+
+        // Vector2 pos = ball_prefab.transform.position;
+        // for(int n=0; n<l_pos.Count; n)
+        // {
+        //     if(pos == l_pos[n])
+        //     {
+        //         // ball_prefab.transform.position = l_pos[index];
+        //         ball_rb2D.velocity = l_pos[index];
+        //         return;
+        //     }
+        // }
     }
     
     void OnMouseDown()
@@ -17,11 +57,24 @@ public class Btn_test_path : MonoBehaviour
         // path_creator.path.AddSegment(new Vector2(5, 5));
 
         print("Click");
-        print("NumSegments " + path_creator.path.NumSegments);
+        // print("NumSegments " + path_creator.path.NumSegments);
         
-        var segment = path_creator.path.GetPointsInSegment(0);
-        print(segment);
-        print(segment[0].x);
-        print(segment[0].y);
+        // var segment = path_creator.path.GetPointsInSegment(0);
+        // print(segment);
+        // print(segment[0].x);
+        // print(segment[0].y);
+
+        // Vector2 vec = new Vector2(1, 1);
+        // ball_rb2D.velocity = vec;
+
+        // ball_prefab.transform.position = l_pos[index];
+        index++;
+        if(index >= l_pos.Count)
+            index = 0;
+
+        //target = l_pos[index];
+        target = new Vector2(5, 5);
+
+        // ball_prefab.velocity = l_pos[index];
     }
 }
