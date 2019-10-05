845cc5f605b86400a28cf938fce6b4c3bf5ccdf6
RinatB2017 <tux4096@gmail.com>
2019-10-05 20:47:42 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/Tests/Calc_path.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calc_path : MonoBehaviour
+{
+    public PathCreator path_creator;
+
+    void OnMouseDown()
+    {
+        print("NumSegments " + path_creator.path.NumSegments);
+    }
+}
