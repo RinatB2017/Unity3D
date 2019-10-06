de59008460576e90c74f60218645dbf3d71abc85
RinatB2017 <tux4096@gmail.com>
2019-10-06 22:01:28 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/Tests/Btn_left_click.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Btn_left_click : MonoBehaviour
+{
+    public GameObject tilemap;
+    bool flag = false;
+
+    void OnMouseDown()
+    {
+        flag =  true;
+    }
+
+    void OnMouseUp()
+    {
+        flag = false;
+    }
+
+    void Update()
+    {
+        if(flag)
+        {
+            tilemap.transform.Rotate(0, 0, 1f, Space.Self);
+        }
+    }
+}
