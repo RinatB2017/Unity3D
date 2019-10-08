e484e78c055ece9fc1a31aea8fa7b8965cb875f3
RinatB2017 <tux4096@gmail.com>
2019-10-09 00:46:20 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/Class_A.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class_A : Class_B
+{
+    public string name_A;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ my_projects/Test/Assets/Scripts/Class_B.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class_B : MonoBehaviour
+{
+    public string name_B;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
