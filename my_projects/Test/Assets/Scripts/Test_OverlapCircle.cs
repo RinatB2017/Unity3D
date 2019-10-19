ee86003334d183b255fd70bc13a6542a3cd59253
RinatB2017 <tux4096@gmail.com>
2019-10-19 12:17:27 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/Test_OverlapCircle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_OverlapCircle : MonoBehaviour
+{
+    void Start()
+    {
+        Vector2 center = new Vector2();
+        center.x = 0f;
+        center.y = 0f;
+        float radius = 3f;
+
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(center, radius);
+        print("hit count " + hitColliders.Length);
+        for(int n=0; n<hitColliders.Length; n++) {
+            Destroy(hitColliders[n].gameObject);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
