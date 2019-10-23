793ec96a47441d820e61e5b836e28ae3dc990cc8
RinatB2017 <tux4096@gmail.com>
2019-10-23 19:47:25 +0300

    update

---
+++ my_projects/Test/Assets/Scripts/LogEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogEvents : MonoBehaviour {
+
+    void print_log (string text) {
+        Debug.Log (text + "\n");
+    }
+
+    void OnEnable () {
+        print_log ("OnEnable");
+    }
+    void OnDisable () {
+        print_log ("OnDisable");
+    }
+    void OnGUI () {
+        print_log ("OnGUI");
+    }
+    void OnApplicationPause () {
+        print_log ("OnApplicationPause");
+    }
+    void OnApplicationQuit () {
+        print_log ("OnApplicationQuit");
+    }
+}
