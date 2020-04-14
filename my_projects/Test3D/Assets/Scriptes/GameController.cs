abde93a4bae246612732dd73dee1b954fe9bb2e1
RinatB2017 <tux4096@gmail.com>
2020-04-14 19:25:38 +0300

    update

---
+++ my_projects/Test3D/Assets/Scriptes/GameController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject sphere_prefab;
+    public float axes_x = 0f;
+    public float axes_y = 1f;
+    public float axes_z = 1f;
+    public float force = 10f;
+
+    public float k_move = 0.1f;
+    public float k_rot = 0.5f;
+
+    private GameObject sphere;
+    private Rigidbody body_sphere;
+    private Vector3 m_NewForce;
+
+    public float runSpeed = 1f;
+    public Joystick rotate_joystick;
+    public Joystick move_joystick;
+
+    private List<GameObject> l_cubes;
+    private List<Vector3> l_vectors;
+
+    private bool f_forward = false;
+    private bool f_back = false;
+
+    Camera m_MainCamera;
+
+    void Start()
+    {
+        m_MainCamera = Camera.main;
+
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cube");
+        print("Cubes " + cubes.Length);
+
+        l_cubes = new List<GameObject>();
+        l_vectors = new List<Vector3>();
+        for(int n=0; n<cubes.Length; n++)
+        {
+            l_vectors.Add(cubes[n].transform.position);
+            l_cubes.Add(cubes[n]);
+        }
+    }
+
+    void move_forward(bool state)
+    {
+        f_forward = state;
+    }
+
+    void move_back(bool state)
+    {
+        f_back = state;
+    }
+
+    void fire(bool state)
+    {
+        if(state)
+        {
+            print("Fire");
+            
+            m_NewForce = m_MainCamera.transform.forward * force;
+
+            Vector3 temp_vector = m_MainCamera.transform.position;
+
+            sphere = Instantiate(sphere_prefab, temp_vector, Quaternion.identity);
+            body_sphere = sphere.GetComponent<Rigidbody>();
+            body_sphere.AddForce(m_NewForce, ForceMode.Impulse);
+        }
+    }
+
+    void Update()
+    {
+        // движение
+        // m_MainCamera.transform.Translate (0,
+        //                                   0,
+        //                                   Input.GetAxis("Vertical")*k_move);
+        
+        m_MainCamera.transform.Translate (move_joystick.Horizontal*k_move, 
+                                          move_joystick.Vertical*k_move,
+                                          0);
+
+        // поворот
+        // m_MainCamera.transform.Rotate(0,
+        //                               Input.GetAxis("Horizontal")*k_rot,
+        //                               0,
+        //                               Space.Self);
+
+        m_MainCamera.transform.Rotate(rotate_joystick.Vertical*k_rot,
+                                      rotate_joystick.Horizontal*k_rot, 
+                                      0,
+                                      Space.Self);
+
+        if(f_forward)
+        {
+            m_MainCamera.transform.Translate (0, 0, k_move);
+        }
+        if(f_back)
+        {
+            m_MainCamera.transform.Translate (0, 0, -k_move);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            print("Jump");
+
+            Quaternion target = Quaternion.Euler(0, 0, 0);
+            for(int n=0; n<l_cubes.Count; n++)
+            {
+                // target = Quaternion.Euler(l_vectors[n]);
+                // print(target);
+                l_cubes[n].GetComponent<Rigidbody>().isKinematic = true;
+                l_cubes[n].transform.position = l_vectors[n];
+                l_cubes[n].transform.rotation = Quaternion.Slerp(l_cubes[n].transform.rotation, 
+                                                                 target,
+                                                                 1);
+                l_cubes[n].GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
+    }
+}
---
+++ my_projects/Test3D/Assets/Scriptes/SpawnWall.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWall : MonoBehaviour
+{
+    public GameObject brick_prefab;
+    public GameObject platform;
+    public GameObject container;
+
+    void test()
+    {
+        Vector3 temp_vector;
+        temp_vector.x = 12.5f;
+        temp_vector.y = 6.5f;
+        temp_vector.z = 17.5f;
+
+        GameObject child = Instantiate(brick_prefab, temp_vector, Quaternion.identity);
+    }
+
+    void Start()
+    {
+        Vector3 pos_platform = platform.transform.position;
+
+        Vector3 temp_vector;
+        temp_vector.x = pos_platform.x;
+        temp_vector.y = pos_platform.y + 1f;
+        temp_vector.z = pos_platform.z;
+
+        int cnt = 0;
+        for(int y=0; y<5; y++)
+        {
+            for(int x=0; x<5; x++)
+            {
+                if(y % 2 == 0)
+                    temp_vector.x = pos_platform.x + x * 2;
+                else
+                    temp_vector.x = pos_platform.x + x * 2 + 1;
+
+                // GameObject child = Instantiate(brick_prefab, temp_vector, Quaternion.identity);
+                GameObject child = Instantiate(brick_prefab, temp_vector, Quaternion.identity, container.transform);
+                // child.GetComponent<Rigidbody>().isKinematic = true;
+                // child.transform.SetParent(container.transform);
+                // проблема - наследуется и масштаб, в результате глюки
+                cnt++;
+            }
+            temp_vector.y += 1f;
+        }
+        print("Cnt " + cnt);
+    }
+
+    void Update()
+    {
+        
+    }
+}
