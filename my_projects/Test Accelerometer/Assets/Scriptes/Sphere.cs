9b40208299b8efa3c9708fcb00f3625faa68d5f2
RinatB2017 <tux4096@gmail.com>
2020-04-14 18:21:21 +0300

    update

---
+++ my_projects/Test Accelerometer/Assets/Scriptes/Sphere.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public GameObject sphere;
+    public float speed = 10.0F;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 dir = Vector3.zero;
+        dir.x = Input.acceleration.x;
+        dir.y = Input.acceleration.y;
+        dir.z = Input.acceleration.z;
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+        
+        dir *= Time.deltaTime;
+        sphere.transform.Translate(dir * speed);        
+    }
+}
