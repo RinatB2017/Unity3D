3dbce037da7ccbe591417f6fbb39098d1409be3e
RinatB2017 <tux4096@gmail.com>
2020-04-17 23:00:36 +0300

    update

--- my_projects/Test SNAPS/Assets/Scripts/Test_show.cs
+++ my_projects/Test SNAPS/Assets/Scripts/Test_show.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Test_show : MonoBehaviour
 {
-    public float angle = .0f;
-    public float angle_delta = 1.0f;
-    public float radius = 5.0f;
-
     public GameObject go;
+    public GameObject planet;
+    public float speed = 30;
+
+    public Vector3 target;
 
     void Start()
     {
@@ -17,23 +17,7 @@ public class Test_show : MonoBehaviour
 
     void Update()
     {
-        if(angle >= 360)
-        {
-            angle = 0;
-        }
-        else
-        {
-            angle += angle_delta;
-        }
-
-        Vector3 pos;
-        // pos.x = radius * Mathf.Sin(angle * Mathf.PI / 180.0f);
-        pos.x = radius * Mathf.Cos(angle * Mathf.PI / 180.0f);
-        pos.y = 0;
-        // pos.z = radius * Mathf.Cos(angle * Mathf.PI / 180.0f);
-        pos.z = radius * Mathf.Sin(angle * Mathf.PI / 180.0f);
-
-        go.transform.position = pos;
-        go.transform.Rotate(0, angle_delta, 0, Space.Self);
+        go.transform.RotateAround(target, Vector3.up, speed * Time.deltaTime);
+        planet.transform.RotateAround(target, Vector3.up, -speed * Time.deltaTime);
     }
 }
