922b046e20eb72594183875ac2c3492495a57690
RinatB2017 <tux4096@gmail.com>
2020-04-17 21:34:10 +0300

    update

---
+++ my_projects/Test SNAPS/Assets/Scripts/Test_show.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_show : MonoBehaviour
+{
+    public float angle = .0f;
+    public float angle_delta = 1.0f;
+    public float radius = 5.0f;
+
+    public GameObject go;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(angle >= 360)
+        {
+            angle = 0;
+        }
+        else
+        {
+            angle += angle_delta;
+        }
+
+        Vector3 pos;
+        pos.x = radius * Mathf.Sin(angle * 3.14f / 180.0f);
+        pos.y = 0;
+        pos.z = radius * Mathf.Cos(angle * 3.14f / 180.0f);
+
+        go.transform.position = pos;
+        go.transform.Rotate(0, angle_delta, 0, Space.Self);
+        // go.transform.Rotate(0, 0, angle_delta, Space.Self);
+        // go.transform.Rotate(angle_delta, 0, 0, Space.Self);
+    }
+}
