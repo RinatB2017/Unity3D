7c8c78dd8781f71d1248267186eea135063e5bea
RinatB2017 <tux4096@gmail.com>
2020-04-17 22:10:21 +0300

    update

--- my_projects/Test SNAPS/Assets/Scripts/Test_show.cs
+++ my_projects/Test SNAPS/Assets/Scripts/Test_show.cs
@@ -27,13 +27,13 @@ public class Test_show : MonoBehaviour
         }
 
         Vector3 pos;
-        pos.x = radius * Mathf.Sin(angle * 3.14f / 180.0f);
+        // pos.x = radius * Mathf.Sin(angle * Mathf.PI / 180.0f);
+        pos.x = radius * Mathf.Cos(angle * Mathf.PI / 180.0f);
         pos.y = 0;
-        pos.z = radius * Mathf.Cos(angle * 3.14f / 180.0f);
+        // pos.z = radius * Mathf.Cos(angle * Mathf.PI / 180.0f);
+        pos.z = radius * Mathf.Sin(angle * Mathf.PI / 180.0f);
 
         go.transform.position = pos;
         go.transform.Rotate(0, angle_delta, 0, Space.Self);
-        // go.transform.Rotate(0, 0, angle_delta, Space.Self);
-        // go.transform.Rotate(angle_delta, 0, 0, Space.Self);
     }
 }
